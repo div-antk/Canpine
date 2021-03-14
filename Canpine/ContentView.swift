5cb9f2c19564a308f7cb9711d4a750b7b9b8d6f3
div-antk <51283468+owaii@users.noreply.github.com>
2021-03-14 19:00:08 +0900

    UI仮置き

--- Canpine/ContentView.swift
+++ Canpine/ContentView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var users: [String] = ["ダニエル", "ゲオルグ", "ミヤギ"]
+    init() {
+        UITableView.appearance().backgroundColor = .clear
+    }
     var body: some View {
-        
+        NavigationView {
+            ZStack {
+                // 背景色
+                Color.yellow
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    List {
+                        ForEach(users, id: \.self) { user in
+                            
+                            HStack(alignment: .center) {
+                                Image(systemName: "person.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100)
+                                    .padding(.all, 20)
+                                VStack(alignment: .leading) {
+                                    Text(user)
+                                        .font(.system(size: 24, weight: .bold))
+                                    Text("東京都")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .foregroundColor(.gray)
+                                    Text("誕生日")
+                                        .font(.system(size: 16, weight: .bold))
+                                        .padding(.top, 8)
+                                }
+                            }.padding(.trailing, 20)
+                            Spacer()
+                        }
+//                        .frame(maxWidth: .infinity, alignment: .center)
+                        .listRowBackground(Color.white)
+//                        .modifier(CardModifier())
+//                        .padding(.all, 10)
+                        
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
