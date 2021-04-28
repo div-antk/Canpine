27e41b4465666c07da4e8d2b6ecd73d0d029efc2
div-antk <51283468+owaii@users.noreply.github.com>
2021-04-28 22:29:02 +0900

    設定画面のUI記述修正

--- Canpine/View/ConfigView.swift
+++ Canpine/View/ConfigView.swift
@@ -14,50 +14,53 @@ struct ConfigView: View {
     @State var status = "飲みたい"
     @State var item = "ビール"
     @Binding var isStandby: Bool
+    @State var paddingBottom: CGFloat = 0
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(Color.white)
-                .frame(height: 280)
+        VStack {
+            Spacer()
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("カンパイを受ける")
+                        .font(.system(size: 12, weight: .bold))
+                        .padding(.horizontal, 40)
+                    ZStack {
+                        Rectangle()
+                            .fill(Color.gray.opacity(0.1))
+                            .frame(height: 42)
+                            .cornerRadius(4)
+                            .padding(.horizontal, 40)
+                        Toggle(isOn: $isStandby, label: {
+                            Text(isStandby ? "はい" : "いいえ")
+                                .font(.system(size: 14, weight: .bold))
+                        })
+                        .padding(.horizontal, 42)
+                    }.padding(.bottom, 8)
+                    VStack(alignment: .leading, spacing: 4) {
+                        // TODO: 状態名を考える
+                        Text("どんなきぶん？")
+                            .font(.system(size: 12, weight: .bold))
+                            .padding(.horizontal, 40)
+                        TextField(status, text: $status)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal, 40)
+                    }
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("飲みもの🍺")
+                            .font(.system(size: 12, weight: .bold))
+                            .padding(.horizontal, 40)
+                        TextField(item, text: $item)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .padding(.horizontal, 40)
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 280)
+                .background(Color.white)
                 .cornerRadius(12)
                 .padding(16)
-                .padding(.bottom, 20)
-            VStack(alignment: .leading, spacing: 8) {
-                Text("カンパイを受ける")
-                    .font(.system(size: 12, weight: .bold))
-                    .padding(.horizontal, 40)
-                ZStack {
-                    Rectangle()
-                        .fill(Color.gray.opacity(0.1))
-                        .frame(height: 42)
-                        .cornerRadius(4)
-                        .padding(.horizontal, 40)
-                    Toggle(isOn: $isStandby, label: {
-                        Text(isStandby ? "はい" : "いいえ")
-                            .font(.system(size: 14, weight: .bold))
-                    })
-                    .padding(.horizontal, 42)
-                }.padding(.bottom, 8)
-                VStack(alignment: .leading, spacing: 4) {
-                    // TODO: 状態名を考える
-                    Text("どんなきぶん？")
-                        .font(.system(size: 12, weight: .bold))
-                        .padding(.horizontal, 40)
-                    TextField(status, text: $status)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.horizontal, 40)
-                }
-                VStack(alignment: .leading, spacing: 4) {
-                    Text("飲みもの🍺")
-                        .font(.system(size: 12, weight: .bold))
-                        .padding(.horizontal, 40)
-                    TextField(item, text: $item)
-                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                        .padding(.horizontal, 40)
-                }
-            }
-            .padding(.bottom, 40)
+                .padding(.bottom, paddingBottom)
+                .animation(.default)
+                Spacer()
+
         }
     }
 }
